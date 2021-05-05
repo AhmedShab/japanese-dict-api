cf6a7e4242dcc56cc27516b22507bb130934e4c4
AhmedShab <hello@ahmedshaaban.tech>
2021-05-06 06:28:48 +1200

    change data strc

--- src/dictionary/dto/create-dictionary.dto.ts
+++ src/dictionary/dto/create-dictionary.dto.ts
@@ -1,12 +1,22 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
+import { Fixture } from 'class-fixtures-factory';
 
 export class CreateDictionaryDto {
   @IsString()
   readonly hiragana: string;
 
   @IsString()
+  readonly sound: string;
+
+  @IsOptional()
+  @Fixture({ type: () => [Object] })
+  readonly examples?: Example[];
+}
+
+export class Example {
+  @IsString()
   readonly english: string;
 
-  @IsString({ each: true })
-  readonly examples?: string[];
+  @IsString()
+  readonly japanese: string;
 }
--- src/dictionary/entities/hiragana.entity.ts
+++ src/dictionary/entities/hiragana.entity.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Example } from '../dto/create-dictionary.dto';
 
 @Schema()
 export class Hiragana extends Document {
@@ -7,10 +8,10 @@ export class Hiragana extends Document {
   hiragana: string;
 
   @Prop()
-  english: string;
+  sound: string;
 
-  @Prop()
-  examples: string[];
+  @Prop({ type: Object })
+  examples: Example[];
 }
 
 export const HiraganaSchema = SchemaFactory.createForClass(Hiragana);
